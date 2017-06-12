95784eefcc27d79a0f79106da51a1830204572d9
Tim Reilly <timreilly@live.com>
2017-06-12 16:04:23 -0700

    no more global scope execution

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// site.js
+
+(function () {
+
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Tim Reilly"
+
+    var main = document.getElementById("main");
+
+    main.onmouseenter = function () {
+        main.style.backgroundColor = "#888";
+    };
+
+    main.onmouseleave = function () {
+        main.style.backgroundColor = ""
+    };
+
+})();
+
+// no longer executing in the global scope because its an anonymous function  
